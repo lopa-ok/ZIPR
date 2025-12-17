a7aebc632783ffee3aa1d78a3c37ab925453c32a
Philopater <lopa@programmer.net>
2025-12-18 00:55:04 +0200

    Wonky movement!

--- Scripts/car.gd
+++ Scripts/car.gd
@@ -1,67 +1,65 @@
-extends Node3D
+extends VehicleBody3D
 
-@onready var Ball = $Ball
-@onready var Car = $Car
-@onready var RightWheel = $"Car/Model/wheel-front-right"
-@onready var LeftWheel = $"Car/Model/wheel-front-left"
-@onready var CarBody = $Car/Model/body
-@onready var Camera = $Camera3D
+var max_engine_force = 300.0
+var max_brake_force = 15.0
+var max_steering = 0.9
+var steering_speed = 2.5
+var body_tilt = 25.0
+
+var max_speed = 50.0
+var speed_steering_factor = 0.4
+
+var camera_distance = 4.0
+var camera_height = 1.8
+var camera_smoothness = 20.0
+var camera_rotation_smoothness = 5.0
+
+@onready var body_mesh = $Model/body
+@onready var wheel_fl = $WheelFrontLeft
+@onready var wheel_fr = $WheelFrontRight
+@onready var wheel_rl = $WheelRearLeft
+@onready var wheel_rr = $WheelRearRight
+@onready var camera = $Camera3D
 
 
-var acceleration = 80.0
-var steering = 18.0
-var turn_speed = 6.0
-var body_tilt = 30
-var camera_distance = 3.5
-var camera_height = 2.0
-var camera_smoothness = 5.0
-
-var max_speed = 40.0
-var grip = 0.88
-
-
-var speed_input = 0 
-var rotate_input = 0
-
+func _ready():
+	if camera:
+		camera.top_level = true
 
 func _physics_process(delta):
-	var car_offset = Vector3(0, -0.5, 0)
-	Car.transform.origin = Ball.transform.origin + car_offset
+	var speed_input = Input.get_axis("Brake", "Accelerate")
+	var steering_input = Input.get_axis("SteerRight", "SteerLeft")
 	
-	var velocity = Ball.linear_velocity
-	var speed = velocity.length()
+	var speed = linear_velocity.length()
 	
-	if speed > max_speed:
-		Ball.linear_velocity = velocity.normalized() * max_speed
+	if speed < max_speed:
+		engine_force = speed_input * max_engine_force
+	else:
+		var speed_factor = clamp(1.0 - (speed - max_speed) / 10.0, 0.0, 1.0)
+		engine_force = speed_input * max_engine_force * speed_factor
 	
-	var forward_velocity = -Car.global_transform.basis.z * velocity.dot(-Car.global_transform.basis.z)
-	var lateral_velocity = Car.global_transform.basis.x * velocity.dot(Car.global_transform.basis.x)
-	Ball.linear_velocity = Ball.linear_velocity.lerp(forward_velocity + lateral_velocity * grip, delta * 12)
+	if speed_input < 0:
+		brake = abs(speed_input) * max_brake_force
+	else:
+		brake = 0.0
 	
-	Ball.apply_central_force(Car.global_transform.basis.z * speed_input)
+	var speed_ratio = clamp(speed / max_speed, 0.0, 1.0)
+	var steering_multiplier = lerp(1.0, speed_steering_factor, speed_ratio)
+	var target_steering = steering_input * max_steering * steering_multiplier
 	
-	var target_position = Car.global_transform.origin - Car.global_transform.basis.z * camera_distance + Vector3.UP * camera_height
-	Camera.global_transform.origin = Camera.global_transform.origin.lerp(target_position, camera_smoothness * delta)
-	Camera.look_at(Car.global_transform.origin + Vector3.UP * 0.5, Vector3.UP)
-
-func _process(delta):
-	speed_input = Input.get_action_strength("Accelerate") * acceleration
+	steering = move_toward(steering, target_steering, delta * steering_speed)
 	
-	if Input.is_action_pressed("Brake"):
-		speed_input -= acceleration * 0.5
+	var t = -steering_input * speed / body_tilt
+	body_mesh.rotation.z = lerp(body_mesh.rotation.z, t, 5.0 * delta)
 	
-	rotate_input = deg_to_rad(steering) * (Input.get_action_strength("SteerLeft") - Input.get_action_strength("SteerRight"))
-	
-	RightWheel.rotation.y = rotate_input
-	LeftWheel.rotation.y = rotate_input
-	
-	var speed = Ball.linear_velocity.length()
-	if speed > 2.0:
-		RotateCar(delta)
-
-func RotateCar(delta):
-	var new_basis = Car.global_transform.basis.rotated(Car.global_transform.basis.y, rotate_input)
-	Car.global_transform.basis = Car.global_transform.basis.slerp(new_basis, turn_speed * delta)
-	
-	var tilt = -rotate_input * Ball.linear_velocity.length() / body_tilt
-	CarBody.rotation.z = lerp(CarBody.rotation.z, tilt, 10 * delta)
+	if camera:
+		var target_pos = global_position - global_transform.basis.z * camera_distance + Vector3.UP * camera_height
+		camera.global_position = camera.global_position.lerp(target_pos, camera_smoothness * delta)
+		
+		var look_target = global_position + Vector3.UP * 0.5
+		var current_look = -camera.global_transform.basis.z
+		var target_direction = (look_target - camera.global_position).normalized()
+		var new_look = current_look.lerp(target_direction, camera_rotation_smoothness * delta).normalized()
+		
+		if new_look.length() > 0.1:
+			camera.look_at(camera.global_position + new_look * 10.0, Vector3.UP)
